059d0bf30324d21c7f95372081c88f548b8add9b
Alain CORVISIER <alain.corvisier@gmail.com>
2019-12-05 17:17:36 +0100

    Custom Error Pages

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -7,11 +7,14 @@ from flaskblog.config import Config
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+
+
 login_manager = LoginManager()
-login_manager.login_view = 'users.login'
-login_manager.login_message_category = 'info'
+login_manager.login_view = "users.login"
+login_manager.login_message_category = "info"
 
 mail = Mail()
+
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -25,8 +28,10 @@ def create_app(config_class=Config):
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
-
+    app.register_blueprint(errors)
+    
     return app
